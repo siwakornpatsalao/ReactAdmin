4ab6d0fb2dd453424e7c8cefa3dab9a98b767548
siwakornpatsalao <siwakorn.pat@ku.th>
2023-08-24 15:38:10 +0700

    delete index2

--- src/layouts/dashboard/config.js
+++ src/layouts/dashboard/config.js
@@ -24,24 +24,6 @@ export const items = [
       </SvgIcon>
     )
   },
-/*   {
-    title: 'Customers',
-    path: '/customers',
-    icon: (
-      <SvgIcon fontSize="small">
-        <UsersIcon />
-      </SvgIcon>
-    )
-  }, */
-/*   {
-    title: 'Companies',
-    path: '/companies',
-    icon: (
-      <SvgIcon fontSize="small">
-        <ShoppingBagIcon />
-      </SvgIcon>
-    )
-  }, */
   {
     title: 'Order',
     path: '/order',
@@ -87,58 +69,4 @@ export const items = [
       </SvgIcon>
     )
   },
-  {
-    title: 'Index2',
-    path: '/index2',
-    icon: (
-      <SvgIcon fontSize="small">
-        <DocIcon />
-      </SvgIcon>
-    )
-  },
-/*   {
-    title: 'Account',
-    path: '/account',
-    icon: (
-      <SvgIcon fontSize="small">
-        <UserIcon />
-      </SvgIcon>
-    )
-  }, */
-  /* {
-    title: 'Settings',
-    path: '/settings',
-    icon: (
-      <SvgIcon fontSize="small">
-        <CogIcon />
-      </SvgIcon>
-    )
-  }, */
-  /* {
-    title: 'Login',
-    path: '/auth/login',
-    icon: (
-      <SvgIcon fontSize="small">
-        <LockClosedIcon />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Register',
-    path: '/auth/register',
-    icon: (
-      <SvgIcon fontSize="small">
-        <UserPlusIcon />
-      </SvgIcon>
-    )
-  }, */
-/*   {
-    title: 'Error',
-    path: '/404',
-    icon: (
-      <SvgIcon fontSize="small">
-        <XCircleIcon />
-      </SvgIcon>
-    )
-  } */
 ];
